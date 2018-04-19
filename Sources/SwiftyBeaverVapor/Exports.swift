4648978104a4f5124e9f49a83a4a6bcd134f1cf0
Nathan Harris <nathanh@l4digital.com>
2018-04-19 16:26:37 -0700

    Refactor Example Code
    - Add: Exports file to simplify usage of SwiftyBeaver with consumers of this package

---
+++ Sources/SwiftyBeaverVapor/Exports.swift
@@ -0,0 +1,5 @@
+//
+// Created by nathanh on 4/19/18.
+//
+
+@_exported import SwiftyBeaver
--- Sources/SwiftyBeaverVaporExample/main.swift
+++ Sources/SwiftyBeaverVaporExample/main.swift
@@ -7,36 +7,46 @@
 //  Some rights reserved: http://opensource.org/licenses/MIT
 //
 
-import Foundation
-import Vapor
 import SwiftyBeaverVapor
-import SwiftyBeaver
+import Vapor
 
-// set-up SwiftyBeaver logging destinations (console, file, cloud, ...)
-// see http://docs.swiftybeaver.com/category/8-logging-destinations to learn more
-let console = ConsoleDestination()  // log to Xcode Console in color
-let file = FileDestination()  // log to file
-file.logFileURL = URL(fileURLWithPath: "/tmp/VaporLogs.log") // set log file
+do {
+    let config = Config.default()
+    let env = try Environment.detect()
+    var services = Services.default()
 
-// add SwiftyBeaver destination instances to SwiftyBeaver Logging Provider
-let sbProvider = SwiftyBeaverProvider(destinations: [console, file])
+    // create and register a test route
+    let router = EngineRouter.default()
+    router.get { request -> String in
+        let log = try request.make(SwiftyBeaverLogger.self)
 
-// create Droplet
-let app = Droplet()
-app.addProvider(sbProvider)
+        log.verbose("not so important")
+        log.debug("something to debug")
+        log.info("a nice information")
+        log.warning("oh no, that won’t be good")
+        log.error("ouch, an error did occur!")
 
-let log = app.log.self // to avoid writing app.log all the time
+        return "Welcome!"
+    }
+    services.register(router, as: Router.self)
 
-// home route
-app.get("/") { request in
-    
-    log.verbose("not so important")
-    log.debug("something to debug")
-    log.info("a nice information")
-    log.warning("oh no, that won’t be good")
-    log.error("ouch, an error did occur!")
-    
-    return "welcome!"
+    // register the SwiftyBeaver Provider with Vapor
+    try services.register(SwiftyBeaverProvider())
+
+    // set-up SwiftyBeaver logging destinations (console, file, cloud, ...)
+    // see http://docs.swiftybeaver.com/category/8-logging-destinations to learn more
+    let console = ConsoleDestination() // log to Xcode Console in color
+    let file = FileDestination() // log to file
+    file.logFileURL = URL(fileURLWithPath: "/tmp/VaporLogs.log") // set log file
+
+    // create the custom SwiftyBeaverConfig and register it with Vapor
+    let myConfig = SwiftyBeaverConfig(destinations: [console, file])
+    services.register(myConfig)
+
+    let app = try Application(config: config, environment: env, services: services)
+
+    try app.run()
+} catch {
+    print(error)
+    exit(1)
 }
-
-app.run()
